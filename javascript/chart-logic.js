574c7a4d5927d0f75f6b6579883761ea069cea6d
khoczkiewicz <serafinbdg@gmail.com>
2021-03-04 20:37:53 +0100

    Initial version of javascript + html5 version of Tank regulator

---
+++ javascript/chart-logic.js
@@ -0,0 +1,123 @@
+// Remarks https://nagix.github.io/chartjs-plugin-streaming/samples/line-horizontal.html
+
+Qd = 30 // Inflow
+Qo = 7 // Outflow
+h = 20 // Level
+output = 0 // Output to compute
+
+var pid = new PID(2, 10, 0.01)
+pid.setSampleTime(100)
+pid.setOutputLimits(0, 60)
+pid.setTarget(20)
+
+var chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+};
+
+function scalingFactor() {
+    output = pid.compute(h)
+    h += Qd * (output / 100) - Qo
+    return h;
+}
+
+function onRefresh(chart) {
+    chart.config.data.datasets.forEach(function(dataset) {
+        dataset.data.push({
+            x: Date.now(),
+            y: scalingFactor()
+        });
+    });
+}
+
+var color = Chart.helpers.color;
+var config = {
+    type: 'line',
+    data: {
+        datasets: [{
+            label: 'PID',
+            backgroundColor: color(chartColors.blue).alpha(0.5).rgbString(),
+            borderColor: chartColors.blue,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            data: []
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Water Tank Regulator'
+        },
+        scales: {
+            xAxes: [{
+                type: 'realtime',
+                realtime: {
+                    duration: 20000,
+                    refresh: 1000,
+                    delay: 2000,
+                    onRefresh: onRefresh,
+                }
+            }],
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Water level'
+                }
+            }]
+        },
+        tooltips: {
+            mode: 'nearest',
+            intersect: false
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: false
+        }
+    }
+};
+
+window.onload = function() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    window.myChart = new Chart(ctx, config);
+};
+
+document.getElementById('randomizeData').addEventListener('click', function() {
+    config.data.datasets.forEach(function(dataset) {
+        dataset.data.forEach(function(dataObj) {
+            dataObj.y = scalingFactor();
+        });
+    });
+    window.myChart.update();
+});
+
+var colorNames = Object.keys(chartColors);
+document.getElementById('addDataset').addEventListener('click', function() {
+    var colorName = colorNames[config.data.datasets.length % colorNames.length];
+    var newColor = chartColors[colorName];
+    var newDataset = {
+        label: 'Dataset ' + (config.data.datasets.length + 1),
+        backgroundColor: color(newColor).alpha(0.5).rgbString(),
+        borderColor: newColor,
+        fill: false,
+        lineTension: 0,
+        data: []
+    };
+
+    config.data.datasets.push(newDataset);
+    window.myChart.update();
+});
+
+document.getElementById('removeDataset').addEventListener('click', function() {
+    config.data.datasets.pop();
+    window.myChart.update();
+});
+
+document.getElementById('addData').addEventListener('click', function() {
+    onRefresh(window.myChart);
+    window.myChart.update();
+});
